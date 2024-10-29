dfd5e1315e91c687c380ffd64f9eeadf3d10172f
JonasOutzen <jonas.outzen.j@gmail.com>
2024-10-29 11:32:54 +0100

    hader git

--- src/main/java/org/example/Cross.java
+++ src/main/java/org/example/Cross.java
@@ -1,4 +1,17 @@
 package org.example;
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.Geometry3D;
 
 public class Cross {
+    JavaCSG csg;
+    public Cross() {
+        this.csg = csg;
+    }
+
+    public void getGeometry() {
+        Geometry3D box1 = csg.box3D(15, 5, 5, false);
+        Geometry3D box2 = csg.box3D(15, 5, 5, false);
+        box2 = csg.rotate3D(csg.degrees(45), csg.degrees(0), csg.degrees(0)).transform(box2);
+
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        JavaCSG csg = JavaCSGFactory.createNoCaching();
+        Cross cross = new Cross();
 
-
+        cross.getGeometry();
     }
 }
